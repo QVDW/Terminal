5a6ed0a12cacb72acb085147902e4db395b04d62
Quinten van de Weg <37804654+QVDW@users.noreply.github.com>
2024-08-19 09:06:52 +0200

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -23,23 +23,23 @@ function examenfolder {
     cd ExamenProject
 }
 
-function qlist{
+function qlist {
     Write-Host "    whodis -  Did u forget someones name?    " -ForegroundColor black -BackgroundColor cyan
     Write-Host "    wya - Shows full path of current location    " -ForegroundColor black -BackgroundColor cyan
 }
 
-function qclone{
+function qclone {
     www
     Write-Host "####################################"-ForegroundColor green
     $ProjectLink = Read-Host "Enter your github clone link"
     $ProjectNaam = Read-Host "What name would you like this project to have? (No spaces)"
     $DatabaseAnswer = Read-Host "Did you create/have a (new) database you can connect? y/n (Default: n)"
-    if ($DatabaseAnswer -eq "y"){
+    if ($DatabaseAnswer -eq "y") {
         git clone $ProjectLink $ProjectNaam
         cd $ProjectNaam
         composer update
     } 
-    else{ 
+    else { 
         
         Write-Host "                            "
         Write-Host "                            " -BackgroundColor red
@@ -50,46 +50,41 @@ function qclone{
     }
     php artisan install
     $ProjectBranchQ = Read-Host "Do you want to work on a new branch or a existing branch? (Default: existing)"
-    if ($ProjectBranchQ -eq "new"){
+    if ($ProjectBranchQ -eq "new") {
         qbranch
     } 
-    else{
+    else {
         qswitch
     }
 
     Write-Host "vergeet de .local niet toetevoegen in de domains van de database" -ForegroundColor red
 }
 
-function qbranch{
+function qbranch {
     $ProjectBranchNew = Read-Host "What would you like to call your new branch?"
     git switch -c $ProjectBranchNew
 }
 
-function qswitch{
+function qswitch {
     allBranch
     $ProjectBranchExisting = Read-Host "To which branch would you like to switch?"
     git switch $ProjectBranchExisting
     $PullQ = Read-Host "Would you like to pull from this branch? y/n (Default: n)"
-    if ($PullQ -eq "y"){
+    if ($PullQ -eq "y") {
         gpull
     } 
-    else{
+    else {
     }
 }
 
-function qcommit{
-    $BranchCommitSwitch = Read-Host "Do you want to move to another branch? (Default: n)"
-    if ($BranchCommitSwitch -eq "y"){
-        allBranch
-        $CommitSwitch = Read-Host "To what branch would you like to commit?"
-        git switch $CommitSwitch  
-    }   
+function qcommit { 
     $CommitMSG = Read-Host "Write a commit message"
-    git commit -am $CommitMSG
+    git add -A
+    git commit -m $CommitMSG
     git push
 }
 
-function qfinalcommit{
+function qfinalcommit {
     $merchRequest = Read-Host "What branch are you gonna make a merge request for?(Default: n)"
     git merge $merchRequest
     $CommitMSG = Read-Host "Write a commit message"
@@ -97,7 +92,7 @@ function qfinalcommit{
     git push
 }
 
-function whodis{
+function whodis {
     Write-Host "   Vincent van Wijk" -ForegroundColor black -BackgroundColor green
     Write-Host "   William Klerk" -ForegroundColor black -BackgroundColor green
     Write-Host "   Vincent Kroon" -ForegroundColor black -BackgroundColor green
@@ -113,35 +108,35 @@ function whodis{
     Write-Host "   Willem (Oudere meneer van redactie)" -ForegroundColor black -BackgroundColor green
 }
 
-function qproject{
+function qproject {
     www
     dir
     $ProjectSwitch = Read-Host "What directory would you like to contineu in?"
     cd $ProjectSwitch
     $ProjectBranchQ = Read-Host "Do you want to work on a new branch or a existing branch? (Default: existing)"
-    if ($DatabaseAnswer -eq "new"){
+    if ($DatabaseAnswer -eq "new") {
         qbranch
     }
-    else{
+    else {
         qswitch
     }
     code .
     $NpmRunDevQ = Read-Host "Do you want to do npm run watch? y/n (Default: n)"
-    if ($NpmRunDevQ -eq "y"){
+    if ($NpmRunDevQ -eq "y") {
         npm run watch
     } 
-    else{
+    else {
     }
 }
 
-function wya{
+function wya {
     Write-Host ""
     Write-Host "You are here: " -NoNewline
     Write-Host $pwd -ForegroundColor white
     Write-Host ""
 }
 
-function www{
+function www {
     cd C:\laragon\www
 }
 
@@ -181,17 +176,17 @@ function gpull { git pull }
 oh-my-posh init pwsh --config 'C:\Users\Quinten\AppData\Local\Programs\oh-my-posh\themes\jblab_2021.omp.json' | Invoke-Expression
 dement
 
-function greset{
+function greset {
     $Restore = Read-Host "Are you sure you want to reset all changes to the last commit? y/n (Default: n)"
-    if ($Restore -eq "y"){
+    if ($Restore -eq "y") {
         git restore .
     } 
-    else{
+    else {
         Write-Host "No changes have been made" -ForegroundColor green
     }
 }
 
-function qmigrations{
+function qmigrations {
     Write-Host "                            "
     Write-Host "                                         " -ForegroundColor black -BackgroundColor cyan
     Write-Host "    qmigNew - Create a new migration     " -ForegroundColor black -BackgroundColor cyan
@@ -201,26 +196,26 @@ function qmigrations{
     Write-Host "                            "
 }
 
-function qmigrateNew{
+function qmigrateNew {
     $MigrationName = Read-Host "What would you like to call your migration?"
     php artisan make:migration $MigrationName
 }
 
-function qmigrate{
+function qmigrate {
     php artisan migrate
 }
 
-function qmigrateRollback{
+function qmigrateRollback {
     $howMany = Read-Host "How many migrations would you like to rollback?"
     php artisan migrate:rollback --step=$howMany
 }
 
-function gdiff{
+function gdiff {
     $DifferenceBetween1 = Read-Host "What difference would be your main branch?"
     $DifferenceBetween2 = Read-Host "What branch would you like to compare it with?"
     git diff $DifferenceBetween1..$DifferenceBetween2
 }
 
-function enablesass{
+function enablesass {
     npm run watch
 }
